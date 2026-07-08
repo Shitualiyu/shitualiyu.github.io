c5bfb8ea8d28f03b3dd9deb9580a5854364c94c0
Shitualiyu <aliyuashitu788@gmail.com>
2026-07-08 10:30:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-// Welcome message
-console.log("Welcome to Shitu Aliyu Abdullahi's Portfolio!");
+const words = [
+    "Aspiring Data Analyst",
+    "Statistics Student",
+    "Microsoft Excel Expert",
+    "Future Data Scientist"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+const typing = document.getElementById("typing");
+
+function type() {
+
+    const current = words[wordIndex];
+
+    if(!deleting){
+        typing.textContent = current.substring(0,charIndex++);
+    }else{
+        typing.textContent = current.substring(0,charIndex--);
+    }
+
+    let speed = 120;
+
+    if(!deleting && charIndex === current.length + 1){
+        deleting = true;
+        speed = 1500;
+    }
+
+    if(deleting && charIndex === 0){
+        deleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+    }
+
+    setTimeout(type,speed);
+}
+
+type();
